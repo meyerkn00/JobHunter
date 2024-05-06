f5d2dc1aaa7a7b998a512d2b6b312492d8a51536
meyerkn00 <karl@themeyers.org>
2024-05-06 13:56:15 -0400

    Using playwright to load page content
    Requests is not a simulated browser so, while faster, cannot handle websites that load their page content from javascript. Also, I downloaded an example of the penn jobs website for reference

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 import requests
+from time import sleep
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
 # Brookings seems to have all of the job search data hidden within javascript. Skipping for now
 # URL = "https://careers-brookings.icims.com/jobs/search?ss=1&hashed=-435682078"
 # page = requests.get(URL)
 
-# Penn Demo
-URL = "https://wd1.myworkdaysite.com/recruiting/upenn/careers-at-penn/"
-page = requests.get(URL)
+# Penn Demo using requests
+# URL = "https://wd1.myworkdaysite.com/recruiting/upenn/careers-at-penn/"
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content, 'lxml')
+# results = soup.find("ul")
 
-soup = BeautifulSoup(page.content, 'lxml')
+#print(soup)
+
+# Penn Demo using playwright
+
+URL = "https://wd1.myworkdaysite.com/recruiting/upenn/careers-at-penn/"
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.goto(URL)
+    sleep(2)
+    soup = BeautifulSoup(page.content(), 'lxml')
+    browser.close()
 
 results = soup.find("ul")
-
-soup.ul
+print(results)
