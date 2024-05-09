931c3b9a12b8913fe5284c212f6e6230151107cf
meyerkn00 <karl@themeyers.org>
2024-05-09 14:11:05 -0400

    Add results to company joblist

--- app.py
+++ app.py
@@ -9,13 +9,7 @@ from playwright.sync_api import sync_playwright
 # URL = "https://careers-brookings.icims.com/jobs/search?ss=1&hashed=-435682078"
 # page = requests.get(URL)
 
-# Penn Demo using requests
-# URL = "https://wd1.myworkdaysite.com/recruiting/upenn/careers-at-penn/"
-# page = requests.get(URL)
-# soup = BeautifulSoup(page.content, 'lxml')
-# results = soup.find("ul")
-
-#print(soup)
+## Outside of central loop definitions
 
 # Output class
 
@@ -37,6 +31,7 @@ class Job:
     link: str
     # location: str
 
+## All of this will go within a loop
 # Penn Demo using playwright
 
 URL = "https://wd1.myworkdaysite.com/recruiting/upenn/careers-at-penn/"
@@ -54,12 +49,19 @@ results = str(soup.ul.find_all("a"))
 result_elements = soup.ul.find_all("a")
 
 # Regex finds job names and links from soup
-#add href="()
+
 links = re.findall(r'href="(/en-US.*?)"', results)
 names = re.findall(r'>([A-Za-z].*?)<', results)
 
-print(links)
-print(names)
-    
-# print([re.sub('<>', "", x) for x in names])
-# print([re.sub('/"', "", x) for x in links])
+# Add first 6 jobs to the joblist of Penn
+Penn = Company("Penn")
+
+for i in range(5):
+    if names[i] == [] or links[i] == [] :
+        x = Job(title = "null", link = "null")
+    else:
+        fulllink = f'https://wd1.myworkdaysite.com{links[i]}'
+        x = Job(title = names[i], link = fulllink)
+    Penn.add_job(x)
+
+print(Penn.joblist)
