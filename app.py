2e55dce0165a97c8df5dda0a7c5d906328034db9
meyerkn00 <karl@themeyers.org>
2024-05-10 12:59:26 -0400

    Implemented html wrap-up and gmail sending

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # import requests
 import re
+from datetime import date
 from time import sleep
 from dataclasses import dataclass
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
+from simplegmail import Gmail
 
 # Brookings seems to have all of the job search data hidden within javascript. Skipping for now
 # URL = "https://careers-brookings.icims.com/jobs/search?ss=1&hashed=-435682078"
@@ -12,18 +14,24 @@ from playwright.sync_api import sync_playwright
 ## Outside of central loop definitions
 
 # Output class
+"""The following class takes a name input and creates
+        1. Joblist to store job results
+        2. function to add to joblist
+        3. bundle function to take all job lists and assemble into string for email
+"""
 
 class Company:
     def __init__(self, name):
         self.name = name
         self.joblist = []
+        self.jobhtml = ''
     
     def add_job(self, joblist):
         self.joblist.append(joblist)
     
-    def bundle(self, joblist):
-        # for each Job, take results and put into text output for email
-        pass
+    def bundle(self):
+        self.jobhtml = ''.join(self.joblist)
+        return(self.jobhtml)
 
 @dataclass
 class Job:
@@ -58,11 +66,36 @@ Penn = Company("Penn")
 
 for i in range(5):
     # This is just to catch if there are less than 6 jobs on a page
-    if names[i] == [] or links[i] == [] :
-        x = Job(title = "null", link = "null")
+    if names[i] == [] or links[i] == []:
+        jobhtml = '<p>None</p><br />'
+        # x = Job(title = "null", link = "null")
     else:
         fulllink = f'https://wd1.myworkdaysite.com{links[i]}'
-        x = Job(title = names[i], link = fulllink)
-    Penn.add_job(x)
+        jobhtml = f'<a href={fulllink}>{names[i]}</a><br />'
+        # x = Job(title = names[i], link = fulllink)
+    Penn.add_job(jobhtml)
 
-print(Penn.joblist)
+# print(Penn.joblist)
+
+# Send results in email
+
+# first time this is run it prompts a login, not sure how to do this on a server
+# If your browser is on a different machine then exit and re-run this
+# application with the command-line parameter
+#  --noauth_local_webserver
+
+gmail = Gmail()
+
+todays_date = date.today()
+
+params = {
+    "to": "***REMOVED***",
+    "sender": "karl0mey@gmail.com",
+    "subject": f'Job Hunter Report {todays_date}',
+    "msg_html": f'<h1>Job Report {todays_date}</h1><br />'\
+                 '<p>Today\'s Job Report is as follows:</p><br />'\
+                 f'<h2>{Penn.name}</h2><br />'\
+                 f'{Penn.bundle()}'
+}
+
+message = gmail.send_message(**params)
