3e23c60b884f59e3defa8878f580064276510e34
meyerkn00 <136020584+meyerkn00@users.noreply.github.com>
2024-06-14 11:50:05 -0400

    Successfully implemented html email body
    see scope https://www.rfc-editor.org/rfc/rfc8621.html

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from playwright.sync_api import sync_playwright
 from simplegmail import Gmail
 import pandas as pd
 import numpy as np
+import sendmail
 
 # Brookings seems to have all of the job search data hidden within javascript. Skipping for now
 # URL = "https://careers-brookings.icims.com/jobs/search?ss=1&hashed=-435682078"
@@ -108,34 +109,31 @@ def comcastanalyze():
 
 # Deprecated - Send results via gmail
 
-def sendgmail(job1):
-    """Sends job results via gmail.
+# def sendgmail(job1):
+#     """Sends job results via gmail.
         
-        currently BROKEN due to api key issue. Google cloud tokens auto-expire after 7 days,
-        which cannot be changed for non-reviewed projects.
+#         currently BROKEN due to api key issue. Google cloud tokens auto-expire after 7 days,
+#         which cannot be changed for non-reviewed projects.
 
-        The first time this runs it will prompt a login.
-        If you are running this remotely use the following command-line param
-            --noauth_local_webserver
-    """
-    gmail = Gmail()
+#         The first time this runs it will prompt a login.
+#         If you are running this remotely use the following command-line param
+#             --noauth_local_webserver
+#     """
+#     gmail = Gmail()
 
-    todays_date = date.today()
+#     todays_date = date.today()
 
-    params = {
-        "to": "karl+jh@themeyers.org",
-        "sender": "karl0mey@gmail.com",
-        "subject": f'Job Hunter Report {todays_date}',
-        "msg_html": f'<h1>Job Report {todays_date}</h1><br />'\
-                    '<p>Today\'s Job Report is as follows:</p><br />'\
-                    f'<h2>{job1.name}</h2><br />'\
-                    f'{job1.bundle()}'
-    }
+#     params = {
+#         "to": "karl+jh@themeyers.org",
+#         "sender": "karl0mey@gmail.com",
+#         "subject": f'Job Hunter Report {todays_date}',
+#         "msg_html": f'<h1>Job Report {todays_date}</h1><br />'\
+#                     '<p>Today\'s Job Report is as follows:</p><br />'\
+#                     f'<h2>{job1.name}</h2><br />'\
+#                     f'{job1.bundle()}'
+#     }
 
-    message = gmail.send_message(**params)
-
-def sendfastmail(job1):
-    pass
+#     message = gmail.send_message(**params)
 
 def savereport():
     """Temp function for saving job results as html.
@@ -146,7 +144,7 @@ def savereport():
     with open(f'TestOutput/{dtm.strftime(dtm.now(),'%m%d%y.%H%M')}.html', 'w', encoding="utf-8") as f:
         f.write(
             '<html><body>'
-            f'<h1>Job Report {todays_date}</h1>'\
+                f'<h1>Job Report {todays_date}</h1>'\
                 '<p>Today\'s Job Report is as follows:</p>'\
                 '<h2>University of Pennsylvania</h2>'\
                 f'{pennanalyze()}'\
@@ -158,6 +156,20 @@ def savereport():
         )
         f.close()
         
+def createhtmlbody():
+    todays_date = date.today()
+    html =  ('<html><body>'
+            f'<h1>Job Report {todays_date}</h1>'\
+                '<p>Today\'s Job Report is as follows:</p>'\
+                '<h2>University of Pennsylvania</h2>'\
+                f'{pennanalyze()}'\
+                '<h2>Brookings Institution</h2>'\
+                f'{brookanalyze()}'\
+                '<h2>Comcast</h2>'\
+                f'{comcastanalyze()}'
+            '</body></html>')
+    return html
+        
 # Main:
 
-savereport()
+sendmail.email_send("karl+jh@themeyers.org", createhtmlbody())
