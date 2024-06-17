9972331b9621c7a48e7aef36f85072223a244164
meyerkn00 <karl@themeyers.org>
2024-06-17 12:24:32 -0400

    WIP Database implementation

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 import pandas as pd
 import numpy as np
+# Custom module, see sendmail.py in main folder
 import sendmail
 
 ## Function Definitions
