db36109b9c5f2482ed92415f2c42e03ba0c1d96f
meyerkn00 <karl@themeyers.org>
2024-06-26 12:22:14 -0400

    Create clearjobs
    Used to clean out job database if things get messy

---
+++ clearjobs.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import Optional, Iterator, Any
+
+import os
+import sys
+import time
+import datetime
+import apsw
+import apsw.ext
+import random
+import re
+import itertools
+from pathlib import Path
+import apsw.bestpractice
+
+apsw.bestpractice.apply(apsw.bestpractice.recommended)
+
+connection = apsw.Connection("job_listings.sqlite3")
+cursor = connection.cursor()
+
+# This file CLEARS the job database. Only run if you want to do that.
+
+cursor.execute("""DELETE FROM "Job_Entries" WHERE ("rowid" > 0)""")
