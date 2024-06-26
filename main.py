6e331c65cc34b45929a79fcda04e8213bcc68df9
meyerkn00 <karl@themeyers.org>
2024-06-26 11:53:49 -0400

    Implemented DB into script
    Created new loop (in main) that iterates through Users, Companies, and Jobs to create job report

--- main.py
+++ main.py
@@ -23,12 +23,26 @@ methods.brookings_job_update() # ID 3
 
 # Pull User ID List (for now just me)
 # maybe add the check to only pull IDs with x >= 1 company attached
-userid_list = [1] # database.get_userids()
+userid_list = database.get_userids()
+user_emails = [x[1] for x in userid_list]
 
-for u_id in userid_list:
+for u_id in [x[0] for x in userid_list]:
+    todays_date = date.today()
     user_keywords = database.get_userkeywords(u_id)
     user_companies = database.get_usercompanies(u_id)
-    
+    company_names = [x[1] for x in user_companies]
+    html = ['<html><body>',
+            f'<h1>Job Report {todays_date}</h1>',
+            '<p>Today\'s Job Report is as follows:</p>'
+            ]
     for c_id in [x[0] for x in user_companies]:
-            print(database.get_recentjobs(c_id, user_keywords))
+            job_tuples = database.get_recentjobs(c_id, user_keywords)
+            html.append(f'<h2>{company_names[c_id - 1]}</h2>')
+            if job_tuples == []:
+                  html.append('<p>No Jobs Found Matching Keywords</p>')
+            else:
+                for i in job_tuples:
+                    html.append(f'<a href={i[1]}>{i[0]}</a><br />')
+    html.append('</body></html>')
+    sendmail.email_send(user_emails[u_id - 1], ''.join(html))
 
