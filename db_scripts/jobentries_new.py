9077209320a1d2347cfcb393177f77e3011e5053
meyerkn00 <karl@themeyers.org>
2024-06-28 13:04:07 -0400

    Debugging db job dupes
    Recreated Job_Entries table with new unique key

---
+++ db_scripts/jobentries_new.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+import apsw
+import apsw.ext
+import apsw.bestpractice
+
+apsw.bestpractice.apply(apsw.bestpractice.recommended)
+
+connection = apsw.Connection("job_listings.sqlite3")
+cursor = connection.cursor()
+
+# Code ran to rebuilt Job_Entries database to fix unique key and foreign key issues. Also removed user.id
+
+cursor.execute("DROP TABLE Job_Entries")
+cursor.execute("""
+               CREATE TABLE 'Job_Entries' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+							'title' TEXT, 
+							'url' TEXT , 
+							'company_id' INTEGER, 
+							'last_update' DATETIME DEFAULT CURRENT_TIMESTAMP, 
+							UNIQUE (`title`, `url`, `company_id`), 
+							FOREIGN KEY (company_id) REFERENCES Companies (id) )
+               """)
