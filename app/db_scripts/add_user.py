94d2c6ad9b0c92a621f53b25ce7fd5b38fc44315
meyerkn00 <karl@themeyers.org>
2024-07-09 14:10:24 -0400

    WIP on script to add new users

---
+++ app/db_scripts/add_user.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import apsw
+import apsw.ext
+import apsw.bestpractice
+
+apsw.bestpractice.apply(apsw.bestpractice.recommended)
+
+connection = apsw.Connection("job_listings.sqlite3")
+cursor = connection.cursor()
+
+user_email = "***REMOVED***"
+user_keywords = ['Data', 'Research', 'Analyst']
+user_companies = [1, 2, 3]
+
+user_id = cursor.execute(
+    "INSERT INTO Users ('email')"
+    f"    VALUES ({user_email})"
+    "     RETURNING Users.id"
+)
+
+# WIP, need to batch insert and pull user id first
+uc_query = (
+    "INSERT INTO User_Companies ('user_id', 'company_id')"
+    f"     VALUES (?, ?)"
+)
+cursor.executemany(uc_query, )
+
+# WIP, need to batch insert
+key_query = (
+    "INSERT INTO User_Keywords ('user_id', 'keyword')"
+    f"     VALUES (?, ?)"
+)
