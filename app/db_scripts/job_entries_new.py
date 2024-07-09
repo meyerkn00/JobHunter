0a2b9403903d1f21039dda7566bcff0cab9d6416
meyerkn00 <karl@themeyers.org>
2024-07-09 14:10:24 -0400

    WIP on script to add new users

--- app/db_scripts/job_entries_new.py
+++ app/db_scripts/job_entries_new.py
@@ -10,13 +10,13 @@ cursor = connection.cursor()
 
 # Code ran to rebuilt Job_Entries database to fix unique key and foreign key issues. Also removed user.id
 
-cursor.execute("DROP TABLE Job_Entries")
-cursor.execute("""
-               CREATE TABLE 'Job_Entries' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-							'title' TEXT, 
-							'url' TEXT , 
-							'company_id' INTEGER, 
-							'last_update' DATETIME DEFAULT CURRENT_TIMESTAMP, 
-							UNIQUE (`title`, `url`, `company_id`), 
-							FOREIGN KEY (company_id) REFERENCES Companies (id) )
-               """)
+# cursor.execute("DROP TABLE Job_Entries")
+# cursor.execute("""
+#                CREATE TABLE 'Job_Entries' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+# 							'title' TEXT, 
+# 							'url' TEXT , 
+# 							'company_id' INTEGER, 
+# 							'last_update' DATETIME DEFAULT CURRENT_TIMESTAMP, 
+# 							UNIQUE (`title`, `url`, `company_id`), 
+# 							FOREIGN KEY (company_id) REFERENCES Companies (id) )
+#                """)
