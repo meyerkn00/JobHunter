e5d24a55c90d2bcdeb68042c2f064ad67d70dd82
meyerkn00 <karl@themeyers.org>
2024-07-10 14:43:42 -0400

    created add_user
    Also fixed how users are treated in main

--- app/db_scripts/add_user.py
+++ app/db_scripts/add_user.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import apsw
 import apsw.ext
 import apsw.bestpractice
+import itertools
 
 apsw.bestpractice.apply(apsw.bestpractice.recommended)
 
@@ -12,21 +13,34 @@ user_email = "craig+jh@themeyers.org"
 user_keywords = ['Data', 'Research', 'Analyst']
 user_companies = [1, 2, 3]
 
-user_id = cursor.execute(
-    "INSERT INTO Users ('email')"
-    f"    VALUES ({user_email})"
-    "     RETURNING Users.id"
-)
+def create_user_id(user_email):
+    user_id = connection.execute(
+        "INSERT INTO Users ('email')"
+        f"    VALUES ('{user_email}')"
+        "     RETURNING Users.id"
+    ).get
+    return user_id
 
-# WIP, need to batch insert and pull user id first
-uc_query = (
-    "INSERT INTO User_Companies ('user_id', 'company_id')"
-    f"     VALUES (?, ?)"
-)
-cursor.executemany(uc_query, )
+def add_user_companies(user_id, user_companies):
+    data = [[*t] for t in zip(itertools.repeat(user_id), user_companies)]
+    query = (
+        "INSERT INTO User_Companies ('user_id', 'company_id')"
+        f"     VALUES (?, ?)"
+    )
+    cursor.executemany(query, data)
 
-# WIP, need to batch insert
-key_query = (
-    "INSERT INTO User_Keywords ('user_id', 'keyword')"
-    f"     VALUES (?, ?)"
-)
+def add_keywords(user_id, user_keywords):
+    data = [[*t] for t in zip(itertools.repeat(user_id), user_keywords)]
+    query = (
+        "INSERT INTO Job_User_Keywords ('user_id', 'keyword')"
+        f"     VALUES (?, ?)"
+    )
+    cursor.executemany(query, data)
+
+def add_user(user_email, user_companies, user_keywords):
+    user_id = create_user_id(user_email)
+    add_user_companies(user_id, user_companies)
+    add_keywords(user_id, user_keywords)
+    print(f'Added user: ID = {user_id}')
+
+add_user(user_email, user_companies, user_keywords)
--- app/main.py
+++ app/main.py
@@ -1,30 +1,30 @@
 from datetime import date
 # Custom modules
-import methods.sendmail as sendmail
-import methods.webqueries as webqueries
+import methods.send_mail as send_mail
+import methods.web_queries as web_queries
 import methods.database as database
 
 ## Main Loop
 
 # Update job_listings db
-webqueries.penn_job_update() # ID 1
-webqueries.comcast_job_update() # ID 2
-webqueries.brookings_job_update() # ID 3
+web_queries.penn_job_update() # ID 1
+web_queries.comcast_job_update() # ID 2
+web_queries.brookings_job_update() # ID 3
 
 # Send emails to users
-userid_list = database.get_userids()
-user_emails = [x[1] for x in userid_list]
+userid_dict = dict(database.get_userids())
+# user_emails = [x[1] for x in userid_list]
 
-for u_id in [x[0] for x in userid_list]:
+for u_id in list(userid_dict):
     todays_date = date.today()
     user_keywords = database.get_userkeywords(u_id)
     user_companies = database.get_usercompanies(u_id)
-    company_names = [x[1] for x in user_companies]
+    company_names = [y[1] for y in user_companies]
     html = ['<html><body>',
             f'<h1>Job Report {todays_date}</h1>',
             '<p>Today\'s Job Report is as follows:</p>'
             ]
-    for c_id in [x[0] for x in user_companies]:
+    for c_id in [z[0] for z in user_companies]:
             job_tuples = database.get_recentjobs(c_id, user_keywords)
             html.append(f'<h2>{company_names[c_id - 1]}</h2>')
             if job_tuples == []:
@@ -33,4 +33,4 @@ for u_id in [x[0] for x in userid_list]:
                 for i in job_tuples:
                     html.append(f'<a href={i[1]}>{i[0]}</a><br />')
     html.append('</body></html>')
-    sendmail.email_send(user_emails[u_id - 1], ' '.join(html))
+    send_mail.email_send(userid_dict[u_id], ' '.join(html))
