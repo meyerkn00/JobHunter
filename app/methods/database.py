2bb150fad6851e317f9995a2d1a579825b6f4c21
meyerkn00 <karl@themeyers.org>
2024-08-01 11:00:04 -0400

    Reformatted code
    in keeping with PEP 8 style guidelines

--- app/methods/database.py
+++ app/methods/database.py
@@ -47,28 +47,39 @@ if check != "ok":
 # Needed Queries
 
 def add_to_jobs(company_id, datatable):
-    data = [[*t] for t in zip(datatable['names'], datatable['links'], itertools.repeat(company_id))]
+    data = [
+        [*t] 
+        for t in zip(
+            datatable['names'], datatable['links'], itertools.repeat(company_id)
+        )
+    ]
     query = """INSERT OR IGNORE INTO Job_Entries ("title", "url", "company_id")
                     VALUES (?, ?, ?)"""
     with connection:
         connection.executemany(query, data)
 
 def get_userids():
-    query = ('SELECT U.id, U.email FROM Users U'
-                ' JOIN User_Companies UC ON (UC.user_id = U.id)'
-                ' GROUP BY U.id'
-                ' HAVING COUNT(UC.company_id) > 0')
+    query = (
+        'SELECT U.id, U.email FROM Users U'
+            ' JOIN User_Companies UC ON (UC.user_id = U.id)'
+            ' GROUP BY U.id'
+            ' HAVING COUNT(UC.company_id) > 0'
+    )
     return dict(cursor.execute(query).fetchall())
 
 def get_userkeywords(user_id):
-    query = ('SELECT keyword FROM Job_User_Keywords JUK'
-                f' WHERE JUK.user_id = ?')
+    query = (
+        'SELECT keyword FROM Job_User_Keywords JUK'
+            f' WHERE JUK.user_id = ?'
+    )
     return cursor.execute(query, (user_id,)).get
 
 def get_usercompanies(user_id):
-    query = ('SELECT UC.company_id, C.name FROM User_Companies UC'
-                '  JOIN Companies C ON (C.id = UC.company_id)'
-                f' WHERE UC.user_id = ?')
+    query = (
+        'SELECT UC.company_id, C.name FROM User_Companies UC'
+            '  JOIN Companies C ON (C.id = UC.company_id)'
+            f' WHERE UC.user_id = ?'
+    )
     return cursor.execute(query, (user_id,)).fetchall()
 
 def keyword_list(keywords):
@@ -84,12 +95,14 @@ def get_recent_jobs(company_id, keywords):
     # Note: update with FTS5 searching once I enable it
     one_week_ago = datetime.now(tz=timezone.utc) - timedelta(weeks = 1)
     one_week_ago_str = one_week_ago.strftime("%Y-%m-%d %H:%M:%S")
-    query = ('SELECT JE.title, JE.url FROM Job_Entries JE'
+    query = (
+        'SELECT JE.title, JE.url FROM Job_Entries JE'
             f' WHERE (JE.company_id = ?)'
             f' AND (JE.last_update > ?)'
             f' AND (JE.title LIKE {keyword_list(keywords)})'
             '  ORDER BY last_update DESC'
-            '  LIMIT 10')
+            '  LIMIT 10'
+    )
     result = cursor.execute(query, (company_id, one_week_ago_str)).fetchall()
     if result == None:
         return []
